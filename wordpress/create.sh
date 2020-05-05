5c644a538639ef104e245dd5061c247fd1a43a59
Matt Mcnamee <matt@mcnamee.co>
2020-05-06 08:17:19 +1000

    Update readme formatting

--- wordpress/create.sh
+++ wordpress/create.sh
@@ -333,25 +333,30 @@ cat << 'EOF' >> README.md
 
 ### a) Automated
 
+
 [Run a single command](https://github.com/pvtl/install-scripts/tree/master/wordpress#%EF%B8%8F-setup-the-site-locally)
 
 ### b) Manual
 
 #### 1. Clone this repo
+
 ```bash
 git clone <repo-url>
 ```
 
 #### 2. Copy `.env.example` to `.env` (and add your environment's settings)
+
 ```bash
 cp .env.example .env
 ```
 
 #### 3. Import the DB
 
+
 Once imported: scrub any sensitive data (eg. customer info, credit card tokens etc).
 
 #### 4. Install dependencies (composer, npm)
+
 ```bash
 composer install --ignore-platform-reqs
 ( cd web/app/themes/pvtl-child ; yarn )
@@ -363,6 +368,7 @@ composer install --ignore-platform-reqs
 
 ### Installation
 
+
 Working in the [Pivotal Docker Dev environment](https://github.com/pvtl/docker-dev), you'll need to do the following:
 
 - You'll need `DB_HOST=mysql` in your `.env`
@@ -371,15 +377,18 @@ Working in the [Pivotal Docker Dev environment](https://github.com/pvtl/docker-d
 
 ### Theme
 
+
 For more information on working with this site's theme, please see the README.md from this site's theme directory.
 
 ---
 
 ## Wordpress Plugins
 
+
 Wordpress Plugins are managed through composer.
 
 ### Installing
+
 
 - Visit [WP Packagist](https://wpackagist.org/)
 - Find the plugin (eg. akismet)
@@ -387,9 +396,11 @@ Wordpress Plugins are managed through composer.
 
 ### Updating
 
+
 Simply update the plugin's version number (to the desired version) in `composer.json` and run `composer update`.
 
 ### Removing
+
 
 Simply run `composer remove wpackagist-plugin/plugin-name`
 EOF
