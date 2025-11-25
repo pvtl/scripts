94b7067356764402802379fed35369b0bfb298c6
Erin W <erin.w@pivotalagency.com.au>
2025-11-25 12:15:04 +1100

    Fix "pvtl/wordpress-training:~1.0"

--- wordpress/create.sh
+++ wordpress/create.sh
@@ -128,6 +128,7 @@ composer config repositories.wp-update-watcher git https://github.com/pvtl/wp-up
 composer config repositories.pvtl-sso git https://github.com/pvtl/wordpress-pvtl-sso-plugin
 composer config repositories.pvtl-itsec-login-logs git https://github.com/pvtl/wordpress-itsec-login-logs-plugin
 composer config repositories.wp-gf-spam-filter git https://github.com/pvtl/wp-gf-spam-filter
+composer config repositories.wordpress-training git https://github.com/pvtl/video-training-wp-plugin
 
 git config --global --add safe.directory $SITE_ROOT/web/app/plugins/wp-update-watcher
 git config --global --add safe.directory $SITE_ROOT/web/app/mu-plugins/pvtl-sso
